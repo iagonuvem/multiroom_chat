0d278a581e587e412eaf55724d742758afc988fa
Iago Nuvem <iagonuvem@hotmail.com>
2018-06-07 17:47:07 -0300

    user out message

--- app.js
+++ app.js
@@ -12,14 +12,27 @@ var io = require('socket.io').listen(server);
 app.set('io', io);
 
 io.on('connection' , function(socket){
+    var _apelido = undefined;
     console.log("Usuário Conectou!");
 
     socket.on('disconnect', function(){
+        // Emite para todos usuários do websocket
+        socket.broadcast.emit('msgUserOut', {
+            apelido: _apelido,
+            msg : 'acabou de sair do chat!'
+        });
+
         console.log("Usuário Desconectou!");
     })
 
+    //Preenche dados locais
+    socket.on('fillData', function(data){
+        _apelido = data.apelido;
+    });
+
     // Recebendo do Websocket
     socket.on('sendMsg', function(data){
+        _apelido = data.apelido;
         // Emite a função apenas para o Usuário atual da requisição
         socket.emit('msgUserIn', {
             apelido: data.apelido,
